294bd4a2ebad5d9f0e632694ade002926b29638a
Mohammad Assadi <34172621+DenaSon@users.noreply.github.com>
2026-09-07 15:52:13 +0330

    fix: reset inherited xDeploy database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,25 +2,15 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        $this->call([
-            SourceSeeder::class,
-            TopicSeeder::class,
-            TopicKeywordSeeder::class,
-        ]);
-
+        //
     }
 }
