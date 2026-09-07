6cce92a14d77d56ed67319c85a838b963a773bc2
Mohammad Assadi <34172621+DenaSon@users.noreply.github.com>
2026-09-07 15:51:49 +0330

    chore: replace inherited TIP navigation with Lumy shell

--- resources/views/components/panel/sidebar.blade.php
+++ resources/views/components/panel/sidebar.blade.php
@@ -9,25 +9,8 @@
         flex-col
     "
 >
-
-    {{-- Brand --}}
-    <div
-        class="
-            px-5
-            py-5
-            border-b
-            border-base-300
-        "
-    >
-
-        <div
-            class="
-                flex
-                items-center
-                gap-3
-            "
-        >
-
+    <div class="px-5 py-5 border-b border-base-300">
+        <div class="flex items-center gap-3">
             <div
                 class="
                     size-11
@@ -41,102 +24,30 @@
                     shrink-0
                 "
             >
-
-                <x-icon
-                    name="o-bolt"
-                    class="w-5 h-5"
-                />
-
+                <x-icon name="o-bolt" class="w-5 h-5" />
             </div>
 
             <div>
-
-                <h2
-                    class="
-                        text-lg
-                        font-black
-                        leading-none
-                    "
-                >
-                    TIP
-                </h2>
-
-                <p
-                    class="
-                        hidden
-                        lg:block
-                        text-xs
-                        text-base-content/60
-                        mt-1
-                    "
-                >
-                    Trend Intelligence Platform
+                <h2 class="text-lg font-black leading-none">Lumy</h2>
+                <p class="hidden lg:block text-xs text-base-content/60 mt-1">
+                    Content Intelligence
                 </p>
-
             </div>
-
         </div>
-
     </div>
 
-    {{-- Navigation --}}
     <div class="flex-1 py-3">
-
-        <ul
-            class="
-                menu
-                w-full
-                px-3
-                gap-1
-            "
-        >
-
+        <ul class="menu w-full px-3 gap-1">
             <li>
-
                 <a
                     wire:navigate
                     wire:current="menu-active"
-                    href="{{ route('panel.opportunities.index') }}"
+                    href="{{ route('dashboard') }}"
                 >
-
-                    <x-icon
-                        name="o-light-bulb"
-                        class="w-5 h-5"
-                    />
-
-                    <span>
-                        فرصت‌ها
-                    </span>
-
+                    <x-icon name="o-squares-2x2" class="w-5 h-5" />
+                    <span>داشبورد</span>
                 </a>
-
             </li>
-
-            <li>
-
-                <a
-                    wire:navigate
-                    wire:current="menu-active"
-                    href="{{ route('panel.trends.index') }}"
-                >
-
-                    <x-icon
-                        name="o-fire"
-                        class="w-5 h-5"
-                    />
-
-                    <span>
-                        روندها
-                    </span>
-
-                </a>
-
-            </li>
-
         </ul>
-
     </div>
-
-
-
 </aside>
